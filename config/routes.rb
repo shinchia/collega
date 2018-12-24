1ab85dea31cf27fffffdac516ceb15d33d371615
shinta <shinchia67020515@gmail.com>
2018-12-24 18:03:57 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'home/create' => 'home#create'
   post 'posts/create' => 'post#create', as: :post_create
   delete 'topics/delete/:id' => 'home#delete', as: :topic_delete
+  delete 'post/delete/:id' => 'post#delete', as: :post_delete
   get 'post/top'
 
 
