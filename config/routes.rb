3ab6903663e73cbfb889a5b39da19ccb8a07c677
shinta <shinchia67020515@gmail.com>
2019-01-01 18:48:22 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get '/' => 'home#top'
   get 'home/show/:id' => 'home#show', as: :topics_show
   post 'home/create' => 'home#create'
   post 'posts/create' => 'post#create', as: :post_create
